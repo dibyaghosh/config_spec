698e9087c238afc0df407c3e27347ef45acd786a
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-12-18 14:18:01 -0800

    Fix name

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="codesave",
-    version="0.0.4",
+    name="config_spec",
+    version="0.0.1",
     packages=find_packages(),
-    entry_points={"console_scripts": [
-        "codesave=codesave.app:codesave_app",
-        "code_from_wandb=codesave.app:wandb_app",
-        "make_pyz=codesave.app:make_pyz_app"
-    ]},)
+)
